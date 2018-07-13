b34933139e1ee0b20ebc1f1ad410bfa654a35a76
jenny <jenny@winlab.rutgers.edu>
2018-07-13 16:34:31 -0400

    added class to encapsulate parts of picar
    class holds front and back wheels, and has a function called go, to
    handle car functionality previously performed in main loop of driver.py.
    Class also has calibration mode, which is toggled through flags
    registered using Observer class.

---
+++ cars/PiCar/car.py
@@ -0,0 +1,48 @@
+from observer import *
+
+sys.path.append('/home/pi/Sunfounder_PiCar')
+import picar
+import picar.front_wheels, picar.back_wheels
+from picar.SunFounder_PCA9685 import Servo
+
+
+class car(Observer):
+    def __init__(self):
+        picar.setup() 
+        self.bw=picar.back_wheels.Back_Wheels()
+        self.fw=picar.front_wheels.Front_Wheels()
+        self.fw.turning_max=40 
+        self.fw.offset=0 
+        self.bw.speed=0
+        self.fw.turn(90)
+        self.calib_mode=False
+        self.observe("calib_start", self.calib_start)
+        self.observe("calib_stop", self.calib_stop)
+        self.observe("calib_left", self.cali_left)
+        self.observe("calib_right", self.cali_right)
+
+    def go(self, STR, THR):
+        if self.calib_mode==False:
+            if THR==0: 
+                self.bw.speed=0 
+                self.bw.stop() 
+            elif THR<0: 
+                self.bw.backward() 
+                self.bw.speed=abs(THR) 
+            else: 
+                self.bw.forward() 
+                self.bw.speed=THR 
+            self.fw.turn(STR)
+
+    def calib_start(self, flag):
+        self.calib_flag=True
+
+    def calib_stop(self, flag):
+        self.calib_flag=False
+        self.front_wheels.cali_ok()
+
+    def cali_left(self, flag):
+        self.front_wheels.cali_left()
+
+    def cali_right(self, flag):
+        self.front_wheels.cali_right()
